49211e2fd680f0f5128a7d178116640e212abdaa
dizelrr <dizel-rr@mail.ru>
2023-10-07 19:59:09 +0300

    example4.2

--- Example/Example004/Program.cs
+++ Example/Example004/Program.cs
@@ -1,3 +1,6 @@
-﻿double numberA = 10;
-double numberB = 5;
-Console.WriteLine(numberA / numberB);
+﻿int numberA = new Random().Next(1, 10);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
+int result = numberA + numberB;
+Console.WriteLine(result);
