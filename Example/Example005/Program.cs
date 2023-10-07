7f72261c555c90d3af40fae971ed5a7aa0bbf5f4
dizelrr <dizel-rr@mail.ru>
2023-10-07 20:10:35 +0300

    example5

---
+++ Example/Example005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя ");
+string username = Console.ReadLine();
+
+if(username == "Маша")
+{
+    Console.WriteLine("Ура Маша");
+}
+else
+{
+    Console.WriteLine("Привет, ");
+    Console.WriteLine(username);
+}
