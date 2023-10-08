35d8295d9f128e8d81139aa26e097d801b6f9ce6
dizelrr <dizel-rr@mail.ru>
2023-10-08 06:41:30 +0300

    урок 1 задача 4

---
+++ Example/Lesson1/Task004/Program.cs
@@ -0,0 +1,12 @@
+﻿// Другой способ показать по числу день недели
+Console.WriteLine("Введите число от 1 до 7 ");
+int num1  = int.Parse(Console.ReadLine());
+string []days = {"пн","вт","ср","чет","пят","сб","вс"};
+if (num1>=1 && num1<=7)
+{
+    Console.WriteLine(days[num1-1]);
+}
+else
+{
+    Console.WriteLine("Введите число от 1 до 7 ");
+}
