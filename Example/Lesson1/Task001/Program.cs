2ea5c4df9e64e91ec2149f012d2e4ae56637f609
dizelrr <dizel-rr@mail.ru>
2023-10-08 05:41:41 +0300

    поиск квадрата числа

---
+++ Example/Lesson1/Task001/Program.cs
@@ -0,0 +1,6 @@
+﻿// поиск квадрата числа
+
+Console.Write("введите число ");
+int inputData = int.Parse(Console.ReadLine());
+int result = inputData * inputData; 
+Console.WriteLine("квадрат числа = " + result);
