9716f0eeb69d21990d407f7da8b1d5e3a9f856e1
dizelrr <dizel-rr@mail.ru>
2023-10-08 05:59:34 +0300

    урок 1 задача 2

---
+++ Example/Lesson1/Task002/Program.cs
@@ -0,0 +1,8 @@
+﻿// проверка является ли чимло его квадратом
+Console.WriteLine("введите число = ");
+int inputData0 = int.Parse(Console.ReadLine());
+Console.WriteLine("введите число проверить = ");
+int inputData1= int.Parse(Console.ReadLine());
+if (inputData1 == inputData0*inputData0)
+Console.WriteLine("yes");
+    else Console.WriteLine("no");
