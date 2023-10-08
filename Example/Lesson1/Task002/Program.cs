cedf4a17ae6df599d3262a414cb385d9a2fee095
dizelrr <dizel-rr@mail.ru>
2023-10-08 06:26:48 +0300

    урок 1 задача 2 повтор

--- Example/Lesson1/Task002/Program.cs
+++ Example/Lesson1/Task002/Program.cs
@@ -3,6 +3,6 @@ Console.WriteLine("введите число = ");
 int inputData0 = int.Parse(Console.ReadLine());
 Console.WriteLine("введите число проверить = ");
 int inputData1= int.Parse(Console.ReadLine());
-if (inputData1 == inputData0*inputData0)
+if (inputData1 == inputData0*inputData0);
 Console.WriteLine("yes");
     else Console.WriteLine("no");
