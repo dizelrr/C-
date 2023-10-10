a6a369f37727a7f276888b53f64729edfd82e095
dizelrr <dizel-rr@mail.ru>
2023-10-10 09:34:28 +0300

    Поиск максимального числа в массиве

--- Example/Example007/Program.cs
+++ Example/Example007/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿ // Поиск максимального числа в массиве
+
+ int Max(int arg1, int arg2, int arg3)
+{
+ int result = arg1;
+ if(arg2 > result) result = arg2;
+ if(arg3 > result) result = arg3;
+return result;
+}
+
+int[] array = {11, 22, 33, 44, 556, 66 ,77 ,88, 99};
+
+
+int result = Max(
+    Max (array[0], array[1], array[2]),
+    Max(array[3], array[4], array[5]),
+    Max(array[6], array[7], array[8])
+);
+Console.WriteLine(result);
