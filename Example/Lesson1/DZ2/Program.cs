5f2c18221c9ced8e6b64f7d9fec337b9a252eb98
dizelrr <dizel-rr@mail.ru>
2023-10-11 09:12:34 +0300

    Программа которая принимает на вход три числа и выдаёт максимальное из этих чисел.

--- Example/Lesson1/DZ2/Program.cs
+++ Example/Lesson1/DZ2/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Программа которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.WriteLine ("введите первое число");
+string number1 = Console.ReadLine();
+int a = int.Parse(number1);
+Console.WriteLine ("введите первое число");
+string number2 = Console.ReadLine();
+int b = int.Parse(number2);
+Console.WriteLine ("введите первое число");
+string number3 = Console.ReadLine();
+int c = int.Parse(number3);
+
+int max = a;
+
+if (max <  b)
+{
+    max = b;
+}
+if (max <  c)
+{
+    max = c;
+}
+
+Console.WriteLine("Максимальное число = " + max);
