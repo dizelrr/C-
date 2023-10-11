c0fbb9734136863e38a1ccc38d351a1d59321ee6
dizelrr <dizel-rr@mail.ru>
2023-10-11 10:27:06 +0300

    // Программа которая на вход принимает число (number), а на выходе выводит все чётные числа от 1 до number (включительно), разделеные знаком табуляции.

--- Example/Lesson1/DZ4/Program.cs
+++ Example/Lesson1/DZ4/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Программа которая на вход принимает число (number), а на выходе выводит все чётные числа от 1 до number (включительно), разделеные знаком табуляции.
+
+Console.WriteLine("Введите число ");
+string number = Console.ReadLine();
+int N = int.Parse(number);
+int n = 0;
+while (n<N)
+{
+    n=n+2;
+    Console.WriteLine(n);
+}
