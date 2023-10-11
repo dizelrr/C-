041a2cbae8ff081c628a02e413d1afac36c3a02c
dizelrr <dizel-rr@mail.ru>
2023-10-11 09:39:57 +0300

    программа которая на вход принимает число number и выводит, является ли число чётным (делится ли оно на два без остатка

--- Example/Lesson1/DZ3/Program.cs
+++ Example/Lesson1/DZ3/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// программа которая на вход принимает число number и выводит, является ли число чётным (делится ли оно на два без остатка).
+
+Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine());
+
+if (a % 2 == 0)
+{
+    Console.WriteLine("Четное число");
+}
+else
+{
+    Console.WriteLine("НЕ четное число");
+}
