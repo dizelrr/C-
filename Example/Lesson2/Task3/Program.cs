b47f276be987d04df8d8972d9fa3304f01e3864a
dizelrr <dizel-rr@mail.ru>
2023-10-12 09:41:38 +0300

    напишите программу которая выводит случайное число из отрезка 10 до 100 и показыввает и показывает наибольшую цифру числа

--- Example/Lesson2/Task3/Program.cs
+++ Example/Lesson2/Task3/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// напишите программу которая выводит случайное число из отрезка 10 до 100 и показыввает и показывает наибольшую цифру числа
+
+int number = new Random().Next(10,100); // генерирует случайное число и перемещает в переменную намбер 
+Console.WriteLine("число" + number);
+int firstDigit = number / 10;
+int lastDigit = number % 10;
+if (firstDigit > lastDigit)
+    {
+        Console.WriteLine(firstDigit);
+    }
+else
+    {
+    Console.WriteLine(lastDigit);
+    }    
