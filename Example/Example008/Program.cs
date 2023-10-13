a000ef68050a4fed85c32419795f4a888386a94f
dizelrr <dizel-rr@mail.ru>
2023-10-13 18:31:58 +0300

    // Одномерный массив array из n элементов . Нахождение элемента массива равное find

--- Example/Example008/Program.cs
+++ Example/Example008/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Одномерный массив array из n элементов . Нахождение элемента массива равное find
+
+int[] array  = {1,20,34,35,657,45,56,67,345,767,45,4};
+int n = array.Length;
+int find =4;
+int index = 0;
+
+while(index < n)
+    {
+        if ( array[index] == find)
+        {
+            Console.WriteLine(index);
+        }
+        index++; // плюс один или после плюса цифра на сколько 
+    }
