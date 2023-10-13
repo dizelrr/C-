8764ffe315d05cdf2b21b9f1252053dfe63f48ff
dizelrr <dizel-rr@mail.ru>
2023-10-13 19:00:17 +0300

    метод создания массива и его печать

--- Example/Example009/Program.cs
+++ Example/Example009/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// метод создания массива и его печать 
+
+void FillArray(int[] collection)
+
+    {
+        int length = collection.Length;
+        int index = 0;
+        while (index < length)
+        {
+            collection [index] = new Random().Next(1,50);
+            index++;
+        }
+    }
+
+void PrintArray(int[] col)
+    {
+        int count = col.Length;
+        int position = 0;
+        while (position < count)
+        {
+            Console.WriteLine(col[position]);
+            position++;
+        }
+    }
+
+
+
+int[] array = new int [13];
+FillArray(array);
+PrintArray(array);
