f4fe4d88cb2a721723e58e12428ce6d8678fff3c
dizelrr <dizel-rr@mail.ru>
2023-10-14 23:44:57 +0300

    урок 3 задача 2 // программа которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y). swith используется

---
+++ Example/Lesson3/Task3/Program.cs
@@ -0,0 +1,23 @@
+﻿// программа которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y). swith используется
+
+int readQuoter(string message) // Функция для ввода переменных
+    {
+    System.Console.WriteLine(message); // получение данных из строки
+    string value = Console.ReadLine(); //Вызов консольного ввода .Valve - значение которое вводим 
+    return int.Parse(value); // возвращаем переменную в int
+    }
+
+int quoter = readQuoter ( " введите номер четверти ");
+if  (quoter>0 & quoter<5)
+    {
+        switch(quoter)
+        {
+            case 1: System.Console.WriteLine("x>0 & y>0");break;
+            case 2: System.Console.WriteLine("x<0 & y>0");break;
+            case 3: System.Console.WriteLine("x<0 & y<0");break;
+            case 4: System.Console.WriteLine("x>0 & y<0");break;
+            
+        }
+       
+    }
+else System.Console.WriteLine("данные ведены не верно");    
