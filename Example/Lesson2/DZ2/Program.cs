74c26a2d72fcf9e403f85937d12051a28c264b80
dizelrr <dizel-rr@mail.ru>
2023-10-14 07:24:43 +0300

    урок 2 дз 2

---
+++ Example/Lesson2/DZ2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+int ReadInt(string msg)  
+    {
+    Console.WriteLine(msg); 
+    string num = Console.ReadLine();  
+    int number; 
+    number = int.Parse(num);  
+    return number; 
+    }
+int number = ReadInt("Введите  число ");
+while ( number > 999)
+    {
+        number = number/10;
+    }
+if (number > 99 )
+    {
+        int result = number % 10;
+        Console.WriteLine(result);
+    }
+if (number < 100 )
+    {
+        Console.WriteLine("третьей цифры нету");
+    }
