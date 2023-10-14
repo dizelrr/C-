9c39060f1102a5d06e0ff84857d1dc245c923050
dizelrr <dizel-rr@mail.ru>
2023-10-14 06:18:08 +0300

    урок 2 дз 1

---
+++ Example/Lesson2/DZ1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+int ReadInt(string msg)  
+    {
+    Console.WriteLine(msg); 
+    string num = Console.ReadLine();  
+    int number; 
+    number = int.Parse(num);  
+    return number; 
+    }
+int number = ReadInt("Введите трех значное число ");
+if (number>99 & 1000>number)
+    {   
+        int SecondDigit=number/10;
+        int result=SecondDigit%10;
+        Console.WriteLine("вторая цифра " + result);
+    }   
+else
+    Console.WriteLine("чисол не трех значное" );
