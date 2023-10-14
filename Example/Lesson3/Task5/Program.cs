b72795e49d1e0ead767f30000f56e90e16c3ed60
dizelrr <dizel-rr@mail.ru>
2023-10-15 00:17:51 +0300

    урок 3 задача 4 программа которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.

---
+++ Example/Lesson3/Task5/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+
+int ReadInt(string message)
+    {
+    System.Console.WriteLine(message); // Получаем данные из строки.
+    string value = Console.ReadLine(); // Вызов консольного ввода, value - значение, которое вводим.
+    return int.Parse(value);
+    }
+int N = ReadInt("введите положительное число");
+if (N > 0)
+    {
+    int index = 0;
+    while (index < N)
+        {
+        index++;
+        Console.WriteLine (index*index);
+        }
+
+    }
+else Console.WriteLine ("число введено неверно. Введите положительное число");
