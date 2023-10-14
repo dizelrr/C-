a4ba867d1743b5412ed6ef732b65fbd4323965e9
dizelrr <dizel-rr@mail.ru>
2023-10-14 08:35:22 +0300

    урок 3

---
+++ Example/Lesson3/Task1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 или Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+
+int enterCoordinate(string message)
+    {
+    System.Console.WriteLine(message);
+    string value = Console.ReadLine();
+    return int.Parse(value)
+    }
+
+boll 
+int x =
