57067ffabccb1e8446973696b15c10563411e2d4
dizelrr <dizel-rr@mail.ru>
2023-10-15 11:45:06 +0300

    урок 3 дз1  программа которая принимает на вход пятизначное число number и проверяет, является ли оно палиндромом.

---
+++ Example/Lesson3/DZ1/Program.cs
@@ -0,0 +1,19 @@
+﻿// программа которая принимает на вход пятизначное число number и проверяет, является ли оно палиндромом.
+
+Console.Write("Введите число: ");
+string? number = Console.ReadLine();
+
+void CheckingNumber(string number)
+    {
+  if (number[0]==number[4] || number[1]==number[3])
+  {
+    Console.WriteLine($"Ваше число: {number} - палиндром.");
+  }
+  else Console.WriteLine($"Ваше число: {number} - не палиндром.");
+    }
+
+if (number!.Length == 5)
+    {
+        CheckingNumber(number);
+    }
+else Console.WriteLine($"Введи правильное число");
