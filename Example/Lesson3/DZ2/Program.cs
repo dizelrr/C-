7d6ad34e367d4c2d0c92109aca9c7236558ae69e
dizelrr <dizel-rr@mail.ru>
2023-10-15 12:05:07 +0300

    урок 3 дз2 программа  принимает на вход координаты двух точек pointA и pointB в виде массива целых чисел, и возвращает расстояние между ними в 3D пространстве.

---
+++ Example/Lesson3/DZ2/Program.cs
@@ -0,0 +1,27 @@
+﻿// программа  принимает на вход координаты двух точек pointA и pointB в виде массива целых чисел, и возвращает расстояние между ними в 3D пространстве.
+
+int x1 = Coordinate("x", "A");
+int y1 = Coordinate("y", "A");
+int z1 = Coordinate("z", "A");
+int x2 = Coordinate("x", "B");
+int y2 = Coordinate("y", "B");
+int z2 = Coordinate("z", "B");
+
+int Coordinate(string coorName, string pointName)
+{
+    Console.Write($"Введите координату {coorName} точки {pointName}: ");
+    return Convert.ToInt16(Console.ReadLine());
+}
+
+double Decision(double x1, double x2, 
+                double y1, double y2, 
+                double z1, double z2)
+    {
+        return Math.Sqrt(Math.Pow((x2-x1), 2) + 
+                        Math.Pow((y2-y1), 2) + 
+                        Math.Pow((z2-z1), 2));
+    }
+
+double segmentLength =  Math.Round (Decision(x1, x2, y1, y2, z1, z2), 2 );
+
+Console.WriteLine($"Длина отрезка  {segmentLength}");
