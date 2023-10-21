3386216c84a66a0a18e5c7ea6078fbdcd8247a85
dizelrr <dizel-rr@mail.ru>
2023-10-21 08:43:20 +0300

    4 урок 1 задача

---
+++ Example/Lesson4/Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+using System.Globalization;
+
+int Prompt(string message)
+    {
+        System.Console.Write(message);
+        string value = Console.ReadLine();
+        int result = Convert.ToInt32(value);
+        return result;
+    }
+int summ = 0;
+int number = Prompt("Введите цисло для подсчета суммы: ");
+while(number > 0 )
+    {
+        summ = summ + number;
+        number --;
+    }
+Console.WriteLine("Сумма чисел = " + summ);
