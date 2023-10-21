3e663451183bfd00fcd3feca6f850e4cec3ef5ce
dizelrr <dizel-rr@mail.ru>
2023-10-21 20:45:05 +0300

    Example 13 таблица умножения

---
+++ Example/Example other/Example013/Program.cs
@@ -0,0 +1,10 @@
+﻿// цикл таблица умножения
+
+for (int i = 2 ; i <=10 ; i++ )
+    {
+        for (int j = 2; j <=10;  j++ )
+        {
+        System.Console.WriteLine(($"{i} x {j} = {i * j}"));
+        }
+        System.Console.WriteLine(); // искуственный разрыв между строк
+    }
