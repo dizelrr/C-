2a7645ba120e5d86afa3b5c33d2b655d5a110543
dizelrr <dizel-rr@mail.ru>
2023-10-22 05:35:40 +0300

    Example 14 метод замены одного на другое. цифры знаки буквы

---
+++ Example/Example other/Example014/Program.cs
@@ -0,0 +1,28 @@
+﻿// Дан текст , работа с текстом , звмена пробелов в тексте
+
+string text = "Привет мир как дела "
+            + "нормально всё как всегда"
+            + "пусть так будет всегда"
+            + "и ничего не меняется";
+
+string Replace (string text, char oldValue, char newValue) // метод замены одного на другое 
+    {
+        string result = String.Empty;
+
+        int length = text.Length;
+        for (int i = 0 ; i < length; i++ )
+        {
+            if (text[i] == oldValue) result = result + $"{newValue}";
+            else result = result + $"{text[i]}";
+        }
+        return result;
+    }
+
+    string newText = Replace (text, ' ', '|');
+    System.Console.WriteLine(newText);
+    System.Console.WriteLine();
+    newText = Replace (text, 'а', 'А');
+    System.Console.WriteLine(newText);
+    System.Console.WriteLine();
+    newText = Replace (text, 'а', '!');
+    System.Console.WriteLine(newText);
