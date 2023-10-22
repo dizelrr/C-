be82f8c305a02471531ee5d72898a1ce27f1b95a
dizelrr <dizel-rr@mail.ru>
2023-10-22 06:44:43 +0300

    урок 4 начало домашки

---
+++ Example/Lesson4/DZ1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// // 2, 4 -> 16
+
+int ReadInt(string msg)  
+    {
+    Console.WriteLine(msg); 
+    string num = Console.ReadLine();  
+    int number; 
+    number = int.Parse(num);  
+    return number; 
+    }
+int number1 = ReadInt("Введите  число ");
+int number2 = ReadInt("Введите  число ");
---
+++ Example/Lesson4/DZ2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int ReadInt(string msg)  
+    {
+    Console.WriteLine(msg); 
+    string num = Console.ReadLine();  
+    int number; 
+    number = int.Parse(num);  
+    return number; 
+    }
+int Summa( int number)
+    {
+        result = 0;
+        while (number > 0)
+            {
+                result += number%10;
+                number = number/10;
+            }
+                return result;
+    }
+
+
+int number = ReadInt("Введите  число ");
+
+System.Console.WriteLine($"Сумма цифр в числе = {Summa( int number)}");
