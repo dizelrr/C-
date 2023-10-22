7bc968b22a875bd325c1d573330385c3fbf2996a
dizelrr <dizel-rr@mail.ru>
2023-10-22 06:08:56 +0300

    Example 15 создания и сортировка массива

---
+++ Example/Example other/Example015/Program.cs
@@ -0,0 +1,31 @@
+﻿// сортировка массива
+int  [] arr = {1,5,3,6,7,8,3,1}; // массив данные
+
+void PrintArray(int[]array) //метод создания массива
+    {
+        int count = array.Length;
+        for (int i = 0; i < count ; i++)
+        {
+            System.Console.Write($"{array[i]} ");
+        }   
+        System.Console.WriteLine();
+    }
+
+void SelectionSort ( int[] array) // фцункция сортировки массива
+    {
+        for ( int i=0; i<array.Length - 1 ; i++)
+        {
+            int minPosition = i;
+            
+            for (int j = i + 1; j < array.Length; j++)
+            {
+                if(array[j] < array[minPosition]) minPosition = j;
+            }
+            int temporary = array[i];
+            array[i]=array[minPosition];
+            array[minPosition] = temporary;
+        }
+    }    
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);
