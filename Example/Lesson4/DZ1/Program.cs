9c680254c4edc4f24d2bc75be3a0882982900f7d
dizelrr <dizel-rr@mail.ru>
2023-10-22 12:11:38 +0300

    урок 4 начало дз1

--- Example/Lesson4/DZ1/Program.cs
+++ Example/Lesson4/DZ1/Program.cs
@@ -10,5 +10,22 @@ int ReadInt(string msg)
     number = int.Parse(num);  
     return number; 
     }
+int  Step( int number1, int number2)
+    {
+        int step = 1;
+        for ( int i = 0; i < number2 ; i++)
+        {
+            step *= number1;
+        }
+        return step;
+    }    
+
 int number1 = ReadInt("Введите  число ");
-int number2 = ReadInt("Введите  число ");
+int number2 = ReadInt("Введите  число степени ");
+
+    
+System.Console.WriteLine($"число {number1} в степени {number2} = {Step(number1,number2)}");
+
+
+
+
