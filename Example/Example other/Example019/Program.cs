13bdbd19451462a30770f86cd399555434fe1848
dizelrr <dizel-rr@mail.ru>
2023-11-01 22:38:36 +0300

    example 18

---
+++ Example/Example other/Example019/Program.cs
@@ -0,0 +1,24 @@
+﻿// нахождеине факториала
+
+// 1*2*3=6  факториал числа 3
+
+double Factorial (int n)
+{
+    //1 факотриал = 1
+    // 0 факториал = 1
+    if(n==1) return 1;
+    else return n*Factorial(n-1);
+} 
+for (int i=1; i<40; i++)
+System.Console.WriteLine($"{i}! ={Factorial(i)}"); 
+
+
+// число фибаначе
+// f(1)=1
+// f(2)=1
+// f(n)= f(n-1) + f(n-2)
+
+int Fibonaci (int n)
+{
+    if (n =)
+}
