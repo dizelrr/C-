a7bda51ba56bbf01324850c7a1ef4d69a816385e
dizelrr <dizel-rr@mail.ru>
2023-11-04 19:34:26 +0300

    урок 7 дз1

--- Example/Lesson6/DZ1/Program.cs
+++ Example/Lesson6/DZ1/Program.cs
@@ -10,3 +10,44 @@ int numb;
 numb = int.Parse(a);//преобразуем строку в число
 return numb;// возврат из функции
 }
+
+
+int[] GlavArray (int length) // создаем массив и вводим чмисла пользователя
+{
+int[] array = new int[length];
+for ( int i = 0; i < array.Length; i++ )
+{
+    array[i] = ReadInt($"Введите {i+1} элемент");
+}
+    return array;
+}
+
+void PrintArray (int[] array) // метод печать массива
+{
+    for( int i =0 ; i < array.Length; i++)
+    {
+        System.Console.Write($"{array[i]}\t");
+    }
+}
+
+int SummaArray (int[]array)
+    {
+        int count = 0;
+        for (int i=0 ; i< array.Length; i++)
+        {
+            if  (array[i] > 0)
+            {
+                count++;
+            }
+        }    
+        return count;    
+    }
+
+
+
+int length = ReadInt("введите кол-во элементов"); //задаем сколько нам нужно элементов
+int [] array; // создаем маасив
+array = GlavArray(length); // заем его длину
+PrintArray(array); // печатаем массив
+System.Console.WriteLine();
+System.Console.WriteLine($"количество больше 0 = {SummaArray(array)}");
