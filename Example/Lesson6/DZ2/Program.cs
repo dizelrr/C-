5faa6d5ac869762d4f4a0302592b2d252703e136
dizelrr <dizel-rr@mail.ru>
2023-11-10 17:35:52 +0300

    урок 7 дз2 готово

--- Example/Lesson6/DZ2/Program.cs
+++ Example/Lesson6/DZ2/Program.cs
@@ -17,7 +17,18 @@ int y = 1 ;
 int line1 = 1 ;
 int line2 = 2 ;
 
-double [] InputLineData (int numOfLine) //  метод вводим данные прямой 
+double[] lineData1 = InputLineData(line1);
+double[] lineData2 = InputLineData(line2);
+
+if (ValidateLines(lineData1,lineData2))
+{
+    double[] coord = FindCoords(lineData1,lineData2);
+    System.Console.WriteLine($"Точка пересечения уравнений y={lineData1[Coef]}*x+{lineData1[Cons]} и y={lineData2[Coef]}*x+{lineData2[Cons]} ");
+    System.Console.WriteLine($"имееь координаты ({coord[x]}, {coord[y]})");
+}
+
+
+double [] InputLineData (int numOfLine)//  метод вводим данные прямой 
 {
     double [] lineData = new double[2];
     lineData [Coef] = number($"введите коффициент для {numOfLine} прямой ");
@@ -34,7 +45,7 @@ double [] FindCoords (double[] lineData1, double [] lineData2) //метод по
 
 }
 
-bool ValidateLines (double [] lineData1 , double[] lineData2)
+bool ValidateLines (double [] lineData1 , double[] lineData2) // метод проверяем  линии на пересечение
 {
     if (lineData1[Coef] == lineData2[Coef])
         {
